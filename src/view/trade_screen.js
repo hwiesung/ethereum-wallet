fa4a339bafdfa3f3e34162bf837849b7b5ddbaea
hwiesung <hwiesung@gmail.com>
2018-09-11 23:03:03 +0900

    Tab Navigation

---
+++ src/view/trade_screen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class TradeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Trade Coin</Text>
+      </View>
+    );
+  }
+}
---
+++ src/view/wallet_screen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class WalletScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>My wallet List</Text>
+      </View>
+    );
+  }
+}
