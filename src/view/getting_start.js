6c1de1d3a8413438018e89c2ba66000b7858e771
hwiesung <hwiesung@gmail.com>
2018-10-14 19:37:28 +0900

    add LinearGradient

--- src/view/getting_start.js
+++ src/view/getting_start.js
@@ -3,7 +3,7 @@ import { View,  Button, ActivityIndicator, Text} from 'react-native';
 import { observer, inject } from 'mobx-react/native'
 import { firebaseApp } from '../firebase'
 import Toast, {DURATION} from 'react-native-easy-toast'
-
+import LinearGradient from 'react-native-linear-gradient';
 
 @inject("appStore") @observer
 export default class GettingStart extends Component {
@@ -61,13 +61,16 @@ export default class GettingStart extends Component {
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'white' }}>
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'white' }}>
           <Text style={{fontSize:30, color:'black', fontWeight:'bold'}}>ComEx Wallet</Text>
-          <Text style={{marginTop:4, marginBottom:24, fontSize:20, color:'rgb(155,155,155)'}}>Welcome to ComEx !!</Text>
+          <Text style={{marginTop:4, marginBottom:24, fontSize:20}}>Welcome to ComEx !!</Text>
           <View style={{backgroundColor:'red', height:202, width:324}}/>
         </View>
-        <View style={{justifyContent: 'center',alignItems: 'center', borderRadius:12, marginBottom:38, backgroundColor:'rgb(48,110,182)',  width:300, height:58}}  onPress={()=>this.moveCreateWallet()}>
-          <Text style={{color:'white', fontSize:28}}> Getting Start</Text>
+        <LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 0}} colors={['#5da7dc', '#306eb6']} style={{justifyContent: 'center',alignItems: 'center', borderRadius:12, marginBottom:38, backgroundColor:'rgb(48,110,182)',  width:300, height:58}}  onPress={()=>this.moveCreateWallet()}>
+          <Text style={{color:'white', fontSize:20, fontWeight:'bold'}}>Getting Started</Text>
+        </LinearGradient>
 
-        </View>
+
+
+
 
 
       </View>
